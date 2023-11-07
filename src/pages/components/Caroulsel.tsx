5ad51cbcb92682d4b8edf4e801c0b5e5be13b940
Mateus Vieira <habdig7@gmail.com>
2023-11-07 11:30:33 -0300

    dfvb

--- src/pages/components/Caroulsel.tsx
+++ src/pages/components/Caroulsel.tsx
@@ -6,18 +6,26 @@ export default function Caroulsel() {
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <img src="https://blog-static.petlove.com.br/wp-content/uploads/2022/11/30161259/racao-para-cachorro-escolher-Petlove.jpg" className="d-block w-100" alt="Cachorro" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
                         </div>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Lorem</h5>
-                            <p>Ipsum</p>
-                        </div>
+
 
                         <div className="carousel-item">
                             <img src="https://nutricaoesaudeanimal.com.br/wp-content/uploads/2019/07/295782-veja-como-deve-ser-a-racao-adequada-para-filhote-de-gato-780x450.jpg" className="d-block w-100" alt="Gato" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
                         </div>
-
                         <div className="carousel-item">
                             <img src="https://t2.ea.ltmcdn.com/pt/posts/7/0/0/quantidade_diaria_de_comida_para_coelhos_24007_orig.jpg" className="d-block w-100" alt="Coelho" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
                         </div>
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
